de0cdd6669a527af10980fc0a7927701f6ab37ed
cybercoder <vahid.lid@live.com>
2025-11-16 23:53:48 +0000

    Add Initial OVS pkg.

---
+++ pkg/ovs/agent.go
@@ -0,0 +1,56 @@
+package ovs
+
+import (
+	"context"
+	"log"
+
+	ovsModels "github.com/hicompute/histack/pkg/ovs/models"
+	"github.com/ovn-kubernetes/libovsdb/client"
+	"github.com/ovn-kubernetes/libovsdb/model"
+)
+
+type OvsAgent struct {
+	ovsClient client.Client
+}
+
+func CreateOVSagent() (*OvsAgent, error) {
+	dbModel, err := model.NewClientDBModel("Open_vSwitch", map[string]model.Model{
+		"Bridge":    &ovsModels.Bridge{},
+		"Port":      &ovsModels.Port{},
+		"Interface": &ovsModels.Interface{},
+	})
+	dbModel.SetIndexes(map[string][]model.ClientIndex{
+		"Port": {{Columns: []model.ColumnKey{
+			{Column: "external_ids", Key: "iface-id"},
+		}}},
+	})
+	if err != nil {
+		log.Printf("failed to create DB model: %v", err)
+		return nil, err
+	}
+
+	ovsClient, err := client.NewOVSDBClient(
+		dbModel,
+		client.WithEndpoint("unix:/var/run/openvswitch/db.sock"),
+	)
+	if err != nil {
+		log.Printf("failed to create OVS client: %v", err)
+		return nil, err
+	}
+
+	ctx := context.Background()
+	if err := ovsClient.Connect(ctx); err != nil {
+		log.Printf("failed to connect to OVSDB: %v", err)
+		return nil, err
+	}
+	_, err = ovsClient.MonitorAll(ctx)
+	if err != nil {
+		log.Printf("failed to monitor OVSDB: %v", err)
+		return nil, err
+	}
+	return &OvsAgent{ovsClient: ovsClient}, nil
+}
+
+func (oa *OvsAgent) Close() {
+	oa.ovsClient.Disconnect()
+}
