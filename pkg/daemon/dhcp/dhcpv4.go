628bce4f425c79562af4473eacde12b754f14a3e
cybercoder <vahid.lid@live.com>
2025-12-04 17:13:00 +0000

    initial DHCP daemon.

--- pkg/daemon/dhcp/dhcpv4.go
+++ pkg/daemon/dhcp/dhcpv4.go
@@ -1,67 +1,159 @@
 package dhcp
 
 import (
-	"encoding/binary"
 	"net"
+	"os"
+	"strings"
 
 	histack_ipam "github.com/hicompute/histack/pkg/ipam"
+	"github.com/insomniacslk/dhcp/dhcpv4"
+	"github.com/insomniacslk/dhcp/dhcpv4/server4"
 	"k8s.io/klog/v2"
 )
 
-func Start() error {
-	ipam := histack_ipam.New()
-	dhcpV4Server := &DHCPV4Server{
-		ipam: *ipam,
-	}
-	return dhcpV4Server.run()
+type HDHCPV4 struct {
+	ipam   histack_ipam.IPAM
+	server server4.Server
 }
 
-func (d4s *DHCPV4Server) run() error {
-	conn, err := net.ListenPacket("udp4", ":67")
+func Start() error {
+	laddr := &net.UDPAddr{IP: net.IPv4zero, Port: 67}
+
+	ipam := histack_ipam.New()
+	h := &HDHCPV4{ipam: *ipam}
+
+	srv, err := server4.NewServer("br-ext", laddr, h.handler, server4.WithDebugLogger())
 	if err != nil {
-		klog.Errorf("Failed to listen dhcp v4: %v", err)
 		return err
 	}
-	buffer := make([]byte, 1500)
-	for {
-		n, addr, err := conn.ReadFrom(buffer)
-		if err != nil {
-			klog.Errorf("Read error: %v", err)
-			continue
-		}
-
-		go d4s.handlePacket(conn, buffer[:n], addr)
-	}
+	return srv.Serve()
 }
 
-func (d4s *DHCPV4Server) handlePacket(conn net.PacketConn, data []byte, clientAddr net.Addr) {
-	packet, err := d4s.parsePacket(data)
-	if err != nil {
+func (hd4 *HDHCPV4) handler(conn net.PacketConn, peer net.Addr, req *dhcpv4.DHCPv4) {
+	macPrefix := os.Getenv("MAC_PREFIX")
+	if macPrefix == "" {
+		macPrefix = "02"
+	}
+	mac := req.ClientHWAddr.String()
+	if !strings.HasPrefix(mac, macPrefix) {
 		return
 	}
-	klog.Infof("Received %v from MAC:%s via relay:%s", packet.msgType, packet.chaddr, packet.giaddr)
+
+	// Log basic info
+	klog.Infof("=== DHCP %s from %s ===", req.MessageType(), req.ClientHWAddr)
+	klog.Infof("XID: %s CIAddr: %s SIAddr: %s", req.TransactionID, req.ClientIPAddr, req.ServerIPAddr)
+
+	switch req.MessageType() {
+	case dhcpv4.MessageTypeDiscover:
+		hd4.handleDiscover(conn, peer, req)
+	case dhcpv4.MessageTypeRequest:
+		hd4.handleRequest(conn, peer, req)
+	default:
+		klog.Infof("Ignoring DHCP message: %s", req.MessageType())
+	}
 }
 
-func (d4s *DHCPV4Server) parsePacket(data []byte) (*DHCPV4Packet, error) {
-	if len(data) < 240 {
-		return nil, net.InvalidAddrError("packet too short")
-	}
-	p := &DHCPV4Packet{
-		op:     data[0],
-		htype:  data[1],
-		hlen:   data[2],
-		hops:   data[3],
-		xid:    binary.BigEndian.Uint32(data[4:8]),
-		secs:   binary.BigEndian.Uint16(data[8:10]),
-		flags:  binary.BigEndian.Uint16(data[10:12]),
-		ciaddr: net.IP(data[12:16]),
-		yiaddr: net.IP(data[16:20]),
-		siaddr: net.IP(data[20:24]),
-		giaddr: net.IP(data[24:28]),
-	}
-	if p.hlen <= 16 {
-		p.chaddr = net.HardwareAddr(data[28 : 28+p.hlen])
+// ------------------------------------------------------------
+//  SHARED HELPERS (DRY)
+// ------------------------------------------------------------
+
+// buildReply creates a reply packet (Offer or ACK)
+func buildReply(req *dhcpv4.DHCPv4, msgType dhcpv4.MessageType) (*dhcpv4.DHCPv4, error) {
+	reply, err := dhcpv4.NewReplyFromRequest(req)
+	if err != nil {
+		return nil, err
 	}
 
-	return p, nil
+	reply.UpdateOption(dhcpv4.OptMessageType(msgType))
+
+	return reply, nil
+}
+
+// applyCommonOptions sets mask, server ID, routes, etc.
+func applyCommonOptions(pkt *dhcpv4.DHCPv4) {
+	// Subnet mask /24
+	pkt.UpdateOption(dhcpv4.OptSubnetMask(net.CIDRMask(24, 32)))
+
+	// Server IP
+	pkt.ServerIPAddr = net.ParseIP("172.16.17.66")
+	pkt.UpdateOption(dhcpv4.OptServerIdentifier(net.ParseIP("172.16.17.66")))
+	// Classless static routes
+	route1 := &dhcpv4.Route{
+		Dest: &net.IPNet{
+			IP:   net.ParseIP("172.16.22.1"),
+			Mask: net.CIDRMask(32, 32),
+		},
+		Router: net.ParseIP("0.0.0.0"),
+	}
+
+	route2 := &dhcpv4.Route{
+		Dest: &net.IPNet{
+			IP:   net.ParseIP("0.0.0.0"),
+			Mask: net.CIDRMask(0, 32),
+		},
+		Router: net.ParseIP("172.16.22.1"),
+	}
+
+	pkt.UpdateOption(dhcpv4.OptClasslessStaticRoute(route1, route2))
+}
+
+// sendPacket writes the DHCP packet
+func sendPacket(conn net.PacketConn, peer net.Addr, pkt *dhcpv4.DHCPv4) {
+	_, err := conn.WriteTo(pkt.ToBytes(), peer)
+	if err != nil {
+		klog.Errorf("send failed: %v", err)
+	}
+}
+
+// ------------------------------------------------------------
+//              OFFER HANDLER
+// ------------------------------------------------------------
+
+func (hd4 *HDHCPV4) handleDiscover(conn net.PacketConn, peer net.Addr, req *dhcpv4.DHCPv4) {
+	ip, err := hd4.ipam.FindClusterIPbyFamilyandMAC(req.ClientHWAddr.String(), "v4")
+	if err != nil {
+		klog.Errorf("IPAM lookup failed: %v", err)
+		return
+	}
+
+	offer, err := buildReply(req, dhcpv4.MessageTypeOffer)
+	if err != nil {
+		klog.Errorf("Offer reply build failed: %v", err)
+		return
+	}
+
+	offer.YourIPAddr = net.ParseIP(ip.Spec.Address)
+
+	applyCommonOptions(offer)
+
+	klog.Infof("Sending OFFER → %s", offer.YourIPAddr)
+
+	sendPacket(conn, peer, offer)
+}
+
+// ------------------------------------------------------------
+//              REQUEST HANDLER
+// ------------------------------------------------------------
+
+func (hd4 *HDHCPV4) handleRequest(conn net.PacketConn, peer net.Addr, req *dhcpv4.DHCPv4) {
+
+	ip := req.RequestedIPAddress()
+	if ip == nil {
+		klog.Errorf("Client REQUEST had no RequestedIPAddress")
+		return
+	}
+
+	ack, err := buildReply(req, dhcpv4.MessageTypeAck)
+	if err != nil {
+		klog.Errorf("Ack reply build failed: %v", err)
+		return
+	}
+
+	ack.YourIPAddr = ip
+
+	applyCommonOptions(ack)
+
+	klog.Infof("Sending ACK → %s", ack.YourIPAddr)
+
+	sendPacket(conn, peer, ack)
 }
