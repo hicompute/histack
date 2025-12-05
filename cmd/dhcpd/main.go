cee77fd959260e92cc3a0c813b3fbcd17813c2f7
cybercoder <vahid.lid@live.com>
2025-12-05 00:53:06 +0000

    Add estimated flags.

--- cmd/dhcpd/main.go
+++ cmd/dhcpd/main.go
@@ -1,12 +1,29 @@
 package main
 
 import (
+	"flag"
+	"os"
+
 	dhcpv4d "github.com/hicompute/histack/pkg/daemon/dhcp"
 	"k8s.io/klog/v2"
 )
 
 func main() {
-	if err := dhcpv4d.Start(); err != nil {
-		klog.Fatalf("Error on starting dhcp daemon: %v", err)
+	var ifaceName string
+	var serverAddress string
+	var ipFamily string
+	var macPrefix string
+
+	flag.StringVar(&ifaceName, "iface", "br-ext", "The interface to listen for dhcp packets.")
+	flag.StringVar(&serverAddress, "server-address", "0.0.0.0", "The server address.")
+	flag.StringVar(&ipFamily, "ip-family", "v4", "v4/v6")
+	flag.StringVar(&macPrefix, "mac-prefix", "02", "only effects on mac addresses with specific prefix. default 02 ")
+
+	os.Setenv("MAC_PREFIX", macPrefix)
+	if ipFamily == "v4" {
+		os.Setenv("HISTACK_DHCP4_SERVER_ADDRESS", serverAddress)
+		if err := dhcpv4d.Start(ifaceName); err != nil {
+			klog.Fatalf("Error on starting dhcp daemon: %v", err)
+		}
 	}
 }
