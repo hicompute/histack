bb5e4e58be5a323a1c2d466f48e38addbe43248b
cybercoder <cybercoder@cybercoders-Mac-mini.local>
2026-02-01 16:09:49 +0330

    Use actual metric labels.

--- pkg/ovs/eventhandler.go
+++ pkg/ovs/eventhandler.go
@@ -15,20 +15,10 @@ func (h *ovsEventHandler) OnUpdate(table string, old, new model.Model) {
 }
 func (h *ovsEventHandler) OnDelete(table string, m model.Model) {}
 
-func (h *ovsEventHandler) handle(table string, m model.Model) {
+func (h *ovsEventHandler) handle(tabel string, m model.Model) {
 	switch obj := m.(type) {
 
 	case *ovsModels.Interface:
 		h.agent.updateInterfaceStats(obj)
-
-	case *ovsModels.Port:
-		for _, ifaceUUID := range obj.Interfaces {
-			h.agent.ifaceToPort[ifaceUUID] = obj.UUID
-		}
-
-	case *ovsModels.Bridge:
-		for _, portUUID := range obj.Ports {
-			h.agent.portToBridge[portUUID] = obj.Name
-		}
 	}
 }
